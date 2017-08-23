58464dc645032ba2cb8507ceabcb267ba83fcf4d
vagrant <vagrant@homestead>
2017-08-23 21:49:57 +0000

    Got the basics in place for selecting from a db

--- src/API/Blog.hs
+++ src/API/Blog.hs
@@ -4,10 +4,13 @@
 
 module API.Blog where
 
-import Prelude (Eq, Show, String, return)
+import Prelude (Eq, Show, String, Int, return)
+import Control.Monad.IO.Class (liftIO)
 import GHC.Generics (Generic)
 import Data.Aeson.Types (ToJSON)
 import Servant (Application, Server, Proxy(Proxy), Get, JSON, serve, (:>), (:<|>)((:<|>)))
+
+import API.DB (test)
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
@@ -15,21 +18,26 @@ userAPI = Proxy
 data User = User
   { name :: String
   , email :: String
+  , age :: Int
   } deriving (Eq, Show, Generic)
 
 instance ToJSON User
 
 isaac :: User
-isaac = User "Isaac Newton" "isaac@newton.co.uk"
+isaac = User "Isaac Newton" "isaac@newton.co.uk" 112
 
 albert :: User
-albert = User "Isaac Newton" "isaac@newton.co.uk"
+albert = User "Isaac Newton" "isaac@newton.co.uk" 122
 
-type UserAPI = "users" :> Get '[JSON] [User] :<|> "albert" :> Get '[JSON] User :<|> "isaac" :> Get '[JSON] User
+type UserAPI = "users" :> Get '[JSON] [User]
+  :<|> "albert" :> Get '[JSON] User
+  :<|> "isaac" :> Get '[JSON] User
 
 server :: Server UserAPI
 server = return [isaac, albert]
-  :<|> return albert
+  :<|> do
+    res <- liftIO test
+    return (User "Isaac Newton" "Blue" res)
   :<|> return isaac
 
 app :: Application
